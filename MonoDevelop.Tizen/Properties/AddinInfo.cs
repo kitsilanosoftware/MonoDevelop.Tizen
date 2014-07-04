d60962d382bea6ff3555dc71803601b1f005344d
Damien Diederen <dd@crosstwine.com>
2014-07-04 17:31:25 +0200

    Initial commit: Skeleton generated by MonoDevelop's Addin Maker

---
+++ MonoDevelop.Tizen/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"MonoDevelop.Tizen", 
+	Namespace = "MonoDevelop.Tizen",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("MonoDevelop.Tizen")]
+[assembly:AddinCategory ("MonoDevelop.Tizen")]
+[assembly:AddinDescription ("MonoDevelop.Tizen")]
+[assembly:AddinAuthor ("dash")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
