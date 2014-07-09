b5a2ebfc54772d136c3387ed068b03e38c6ab417
Damien Diederen <dd@crosstwine.com>
2014-07-09 14:36:14 +0200

    Metadata: Fix AddinName, which is presented to the user by the add-in manager
    Closes #13.

--- MonoDevelop.Tizen/Properties/AddinInfo.cs
+++ MonoDevelop.Tizen/Properties/AddinInfo.cs
@@ -8,7 +8,7 @@ using Mono.Addins.Description;
 	Version = "1.0.0"
 )]
 
-[assembly:AddinName ("MonoDevelop.Tizen")]
+[assembly:AddinName ("Tizen development")]
 [assembly:AddinCategory ("Mobile Development")]
 [assembly:AddinDescription ("Support for developing and deploying LGPLv2-compliant applications using Mono for Tizen. If your application is not LGPLv2-compliant then you will need to purchase a commercial license from Xamarin. See http://xamarin.com/licensing.")]
 [assembly:AddinAuthor ("Kitsilano Software Inc.")]
